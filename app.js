cbebf3ec191118337a8535b7a94151c578155597
Marcos Lavarello <Marcos.Lavarello.Cambaceres@gds.ey.com>
2022-10-20 17:42:42 -0300

    mobile update - responsive index

---
+++ app.js
@@ -0,0 +1,13 @@
+let closeBtn = document.getElementById('close-menu-btn');
+
+let mobileMenu = document.getElementById('mobile-menu');
+
+let burguerMenu = document.getElementById('burguer-menu');
+
+closeBtn.addEventListener('click', function(){
+    mobileMenu.style.right="-200%"
+})
+
+burguerMenu.addEventListener('click', function(){
+    mobileMenu.style.right="0"
+})
