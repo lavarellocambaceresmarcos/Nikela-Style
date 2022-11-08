45489364bd2599e4051c4183a6c3b8174b11d46f
Marcos Lavarello <Marcos.Lavarello.Cambaceres@gds.ey.com>
2022-11-08 00:51:58 -0300

    new update

--- app.js
+++ app.js
@@ -7,7 +7,23 @@ let burguerMenu = document.getElementById('burguer-menu');
 closeBtn.addEventListener('click', function(){
     mobileMenu.style.right="-200%";
 })
-
 burguerMenu.addEventListener('click', function(){
     mobileMenu.style.right="0"
+})
+
+let searhbar = document.getElementById('search-bar');
+let searchBtn = document.getElementById('search-btn');
+let titles = document.getElementsByClassName('item-title');
+
+searchBtn.addEventListener('click', function(){
+    // alert(searhbar.value)
+
+    for(let i=0; i<titles.length;i++){
+
+        if(titles[i].innerHTML.toLowerCase().includes(searhbar.value)){
+            titles[i].parentElement.style.display="flex"
+        }else {
+            titles[i].parentElement.style.display="none"
+        }
+    }
 })
